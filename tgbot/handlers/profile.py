81f543c03f3b2ccc084f31171e773a26c4989fe5
printeromg <PrinterOMG@ya.ru>
2023-08-11 09:57:14 +0300

    Iiko user update in API when changing name in profile

--- tgbot/handlers/profile.py
+++ tgbot/handlers/profile.py
@@ -6,6 +6,7 @@ from tgbot.handlers.main_menu import send_profile
 from tgbot.keyboards import reply_keyboards
 from tgbot.misc import callbacks, states, messages
 from tgbot.services.database.models import IikoUser
+from tgbot.services.iiko.schemas import CreateOrUpdateCustomer
 from tgbot.services.utils import contains_only_russian_letters
 
 
@@ -31,7 +32,9 @@ async def get_new_name(message: Message, state: FSMContext):
         iiko_user = await IikoUser.get_by_telegram_id(session, message.from_id)
         iiko_user.name = name
 
-    # TODO: update iiko user in API
+    iiko = message.bot.get('iiko')
+    update_user = CreateOrUpdateCustomer(id=str(iiko_user.id), name=name)
+    await iiko.create_or_update_customer(update_user)
 
     await state.finish()
     await message.answer(messages.name_updated.format(name=name), reply_markup=reply_keyboards.main_menu)
